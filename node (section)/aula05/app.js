f8e2a251a618f786322c0f40a122e60c6ab66be9
Geovanne Washington <geoxp98@gmail.com>
2024-08-11 18:00:06 -0300

    Adding new files

---
+++ node (section)/aula05/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+// console.log(typeof express); //function
+
+//         POST    GET   PUT     DELETE
+// CRUD -> CREATE, READ, UPDATE, DELETE
+
+app.get('/', (req, res) => {
+    res.send(`
+    <form action="/" method="POST">
+    Nome: <input type="text" name="nome">
+    <button>Send</button>
+    </form>
+    `);
+});
+
+app.post('/', (req, res) => {
+    res.send('Form received');
+});
+
+app.get('/contact', (req, res) => {
+    res.send('Thank you for contacting us');
+});
+
+app.listen(3000, () => {
+    console.log('Acess http://localhost:3000');
+});
